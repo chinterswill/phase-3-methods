f22381d0054e7eb56080b3445835362dc38905f8
Kamran Hussein <chinterswill@gmail.com>
2022-12-12 13:01:36 -0500

    commit tree 0abd7e51328eec10aa58c9fd55a96f9d059efeb3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+
+
+def greet_programmer
+    "Hello, programmer!"
+end
+greet_programmer
+
+
+def greet(name)
+    puts `Hello, #{name}!`
+end
+greet('Naureen')
+greet('Jimmy')
+
+
+def greet_with_default(name = 'programmer')
+    puts `Hello, #{name}!`
+end
+greet_with_default('Naureen')
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+add(2, 5)
+
+def halve(num1)
+    if name.class != Integer 
+        return nil
+    end
+
+    num1 / 2
+end
+halve(6)
+
